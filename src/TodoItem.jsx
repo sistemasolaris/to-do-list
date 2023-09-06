ead3d075656ec5a4a39c095b45d963288910b0db
Solaris <ovvo.system@gmail.com>
2023-09-06 16:31:48 -0300

    feat: toggle task completion

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,9 @@
 import './index.css';
 
-function TodoItem({ completed, text }) {
+function TodoItem({ id, completed, text, toggleCompletion }) {
   return (
     <li className="flex gap-2 items-center">
-      <input type="checkbox" checked={completed} />
+      <input type="checkbox" checked={completed} onChange={e => toggleCompletion(id, e.target.checked)} />
       <label>{text}</label>
       <button className="bg-transparent hover:bg-red-500 text-red-700 text-base font-semibold hover:text-white py-1 px-2 border border-red-500 hover:border-transparent rounded">Delete</button>
     </li>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -15,13 +15,25 @@ function TodoList() {
     });
   }
 
+  function toggleCompletion(id, completed) {
+    setItems((currentItems) => {
+      return currentItems.map((item) => {
+        if (item.id === id) {
+          return { ...item, completed };
+        }
+
+        return item;
+      })
+    })
+  }
+
   return (
     <>
       <h1 className="text-4xl font-bold">Todo List</h1>
       <AddTodoItem addItem={addItem} />
       <ul className="flex flex-col gap-2 max-w-4xl w-full text-xl">
         {items.map((item) => {
-          return <TodoItem key={item.id} completed={item.completed} text={item.text} />;
+          return <TodoItem key={item.id} id={item.id} completed={item.completed} text={item.text} toggleCompletion={toggleCompletion} />;
         })}
       </ul>
     </>
